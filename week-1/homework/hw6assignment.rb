172a2f7986abe1f5c6a59ee0cd3d2e1a5f361ba6
Agnaldo Cardoso Junior <acjunior@agnaldos-mbp.lan>
2021-11-16 08:39:04 +0000

    solves homework assignment 2

--- week-1/homework/hw6assignment.rb
+++ week-1/homework/hw6assignment.rb
@@ -7,22 +7,42 @@ class MyPiece < Piece
   # The constant All_My_Pieces should be declared here
 
   # class array holding all the pieces and their rotations
-    All_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
+    All_My_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
     rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
-    [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
-    [[0, 0], [0, -1], [0, 1], [0, 2]]],
+    [[[0, 0], [-1, 0], [1, 0], [2, 0]], [[0, 0], [0, -1], [0, 1], [0, 2]]], # long (only needs two)
     rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
     rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
     rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
-    rotations([[0, 0], [1, 0], [0, -1], [-1, -1]])] # Z 
-end
+    rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]), # Z
+    rotations([[0, 0], [1, 0], [0, 1], [1, 1], [2, 1]]), # new piece 1
+    rotations([[0, 0], [1, 0], [2,0], [-1, 0], [-2, 0]]), # new piece 2
+    rotations([[0, 0], [0, 1], [1,0], [0, 0]])] # new piece 3
+  
+    def self.next_piece (board)
+      MyPiece.new(All_My_Pieces.sample, board)
+    end
+ end
 
 class MyBoard < Board
+  def initialize (game)
+    @grid = Array.new(num_rows) {Array.new(num_columns)}
+    @current_block = MyPiece.next_piece(self)
+    @score = 0
+    @game = game
+    @delay = 500
+  end
+
   def rotate_clockwise_180
     if !game_over? and @game.is_running?
       @current_block.move(0, 0, 2)
     end
     draw
+  end
+
+    # gets the next piece
+  def next_piece
+    @current_block = MyPiece.next_piece(self)
+    @current_pos = nil
   end
 
 end
